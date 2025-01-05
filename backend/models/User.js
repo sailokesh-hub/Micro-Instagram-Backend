45780c0c88350852931ee3df03da60467f46b539
Sai Lokesh <sailokeshchirumarri@gmail.com>
2025-01-05 09:51:46 +0530

    19th commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,12 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const User = sequelize.define(
-  "User",
-  {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING(256), allowNull: false },
-    mobile_number: { type: DataTypes.INTEGER, allowNull: false, unique: true },
-    address: { type: DataTypes.TEXT },
-    post_count: { type: DataTypes.INTEGER, defaultValue: 0 },
-  }
-);
+const User = sequelize.define("User", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING(256), allowNull: false },
+  mobile_number: { type: DataTypes.STRING, allowNull: false, unique: true },
+  address: { type: DataTypes.TEXT },
+  post_count: { type: DataTypes.INTEGER, defaultValue: 0 },
+});
 
 module.exports = User;
